de5376c88a10a73f5bbefb0da5468acaa56cea7e
Luis Ramirez <luisf350@gmail.com>
2020-07-23 07:21:55 -0500

    Add DataAnnotations validation

--- WebApiTest.API/Models/CourseForCreationDto.cs
+++ WebApiTest.API/Models/CourseForCreationDto.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApiTest.API.Models
 {
     public class CourseForCreationDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [MaxLength(1500)]
         public string Description { get; set; }
 
     }
