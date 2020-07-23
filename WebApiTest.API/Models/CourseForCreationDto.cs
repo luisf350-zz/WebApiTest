1dc29cadcfa5c9b439f00da5a8bf8b37d881d492
Luis Ramirez <luisf350@gmail.com>
2020-07-23 07:28:16 -0500

    Add class level validation

--- WebApiTest.API/Models/CourseForCreationDto.cs
+++ WebApiTest.API/Models/CourseForCreationDto.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace WebApiTest.API.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto : IValidatableObject
     {
         [Required]
         [MaxLength(100)]
@@ -12,5 +13,13 @@ namespace WebApiTest.API.Models
         [MaxLength(1500)]
         public string Description { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult("The provided description should be different from the title.",
+                    new[] { "CourseForCreationDto" });
+            }
+        }
     }
 }
